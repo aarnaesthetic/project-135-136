93f535a90aa4ef703aee7f194a6601a40e79bcbd
aarnaesthetic <96248343+aarnaesthetic@users.noreply.github.com>
2022-09-29 23:55:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-img="";
+
 status="";
 objects=[];
 
@@ -23,50 +23,17 @@ function start()
 
 function draw()
 {
-    image(video,0,0,380,380);
-    if (status !="")
-    {   
-        objectDetector.detect(video,gotResult)
-        r=random(255);
-        g=random(255);
-        b=random(255);
-        for(i=0;i < objects.length; i++ )
-        {
-            document.getElementById("status").innerHTML="Status: Objects Detected";
-            document.getElementById("number-of-objects").innerHTML="Number of Objects: "+ objects.length;
-
-             fill(r,g,b);
-             percent=floor(objects[i].confidence * 100);
-             text(objects[i].label +" "+ percent+ "%", objects[i].x+15 , objects[i].y+15);
-             noFill();
-             stroke(r,g,b);
-             rect(objects[i].x , objects[i].y,objects[i].width , objects[i].height);
-             
-
-
-        
-
-
-        }
-    }
+    image(video,0,0,380,380)
+}
+    
 
   
-}
+
 
 function modelLoaded()
 {
-    console.log("MODEL IS LOADED :D")
+    console.log("your website works now continue doing project -_-")
     status='true';
     
 
-}
-
-function gotResult(error,results)
-{
-    if (error)
-    {
-        console.log(error);
-    }
-    console.log(results)
-    objects= results;
 }
